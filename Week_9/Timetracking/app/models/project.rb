91a7637feaf3a08ae871a7c519cabc1f1151232b
melaniemagdalene <melanieblazevic@icloud.com>
2016-06-25 14:36:15 -0400

    First rails application

---
+++ Week_9/Timetracking/app/models/project.rb
@@ -0,0 +1,26 @@
+# Create a class method called ::clean_old in the Project class that deletes all projects older than 1 week
+# Move the code to obtain the last 10 created projects to a method in the class Project name last_created_projects(n)
+
+class Project < ActiveRecord::Base
+	def self.clean_old
+		where("created_at < ?", 1.week.ago).destroy_all
+	end
+
+	def self.last_created_projects(limit)
+		limit(limit).order(created_at: :desc)
+	end
+end
+
+
+# Example
+
+# class Project
+# 	attr_reader :name
+
+# 	def initialize(name)
+# 		@name = name
+# 	end
+# end
+
+# p = Project.new("Ironhack"
+# p.name
