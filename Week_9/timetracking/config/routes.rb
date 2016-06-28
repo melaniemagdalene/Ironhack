e04f7f1b5550cea189cf81ad7c795b4d3bdbb1b0
melaniemagdalene <melanieblazevic@icloud.com>
2016-06-28 17:20:05 -0400

    Removed folder from repository

--- Week_9/timetracking/config/routes.rb
+++
@@ -1,9 +0,0 @@
-Rails.application.routes.draw do
-  get '/' => 'site#home'
-  get '/contact' => 'site#contact'
-
-  get '/projects' => 'projects#index'
-  get '/projects/new' => 'projects#new'
-  post '/projects' => 'projects#create'
-  get '/projects/:id' => 'projects#show'
-end
