ddae29742f8477ffe7cdc963ddedc59515f887a6
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-02 14:07:13 -0400

    Updated routes

--- Week_10/URL_shortener/config/routes.rb
+++ Week_10/URL_shortener/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
-  get '/' => 'links#new'
-  get '/links/:id' => 'links#show'
-  # as: :links adds form helper
-  post '/links' => 'links#create'
-  get '/:shortlink' => 'links#index'
+	get '/' => 'links#new'
+	
+	resources :links, only: [:create, :show]
+
+	get '/:shortlink' => 'links#index'
 end
