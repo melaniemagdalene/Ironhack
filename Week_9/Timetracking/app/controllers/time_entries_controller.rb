c711eed8867a22b7990b470d0c77fb8fd676c16b
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-02 15:06:48 -0400

    Added create time entries functionality

--- Week_9/Timetracking/app/controllers/time_entries_controller.rb
+++ Week_9/Timetracking/app/controllers/time_entries_controller.rb
@@ -1,4 +1,20 @@
 class TimeEntriesController < ApplicationController
+	
+	# Create action
+	def create
+		# input: params[:project_id] and params[:time_entry]
+		@project = Project.find(params[:project_id])
+		time_entry_params = params.require(:time_entry).permit(
+			:hours, :minutes, :date
+			)
+		# time_entry_params = { hours: 1, minutes: 3, date: somedate }
+		@time_entry = @project.time_entries.new(:time_entry_params)
+		if @time_entry.save
+			redirect_to project_time_entries_path(@project)
+		else
+			render 'new'
+		end
+	end
 
 	def index
 		@project = Project.find(params[:project_id])
