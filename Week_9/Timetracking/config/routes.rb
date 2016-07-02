6c15f08d8941ee22747012fc7c563a667b55edac
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-02 14:06:09 -0400

    Updated routes

--- Week_9/Timetracking/config/routes.rb
+++ Week_9/Timetracking/config/routes.rb
@@ -2,9 +2,8 @@ Rails.application.routes.draw do
   get '/' => 'site#home'
   get '/contact' => 'site#contact'
 
-  get '/projects' => 'projects#index'
-  get '/projects/new' => 'projects#new'
-  post '/projects' => 'projects#create'
-  get '/projects/:id' => 'projects#show'
-  get '/projects/project_id/time_entries' => 'time_entries#index'
+  	# Specify routes to generate
+	resources :projects, only: [:index, :show, :new, :create] do
+		resources :time_entries, only: [:index, :new, :create]
+	end
 end
