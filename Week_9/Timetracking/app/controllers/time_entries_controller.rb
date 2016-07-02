83aad7efd1dee76b06a3e07d63eb0fe3497cf090
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-02 15:51:10 -0400

    Added links to the project

--- Week_9/Timetracking/app/controllers/time_entries_controller.rb
+++ Week_9/Timetracking/app/controllers/time_entries_controller.rb
@@ -27,14 +27,10 @@ class TimeEntriesController < ApplicationController
 	end
 end
 
-	# def create
-		# input: project's id and it'll be in the params hash
-		
-		# 1. Find the project
-			# @project = Project.find(params[:id])
-		# 2. Initialize a new time entry for the project
-			# @time_entry = @project.time_entries.new
-		# 3. Save the time entry
-			# @time_entry.save
-		
-		# outcome: a time entry for the give project is created
+	# Notes
+	# 1. Find the project
+		# @project = Project.find(params[:id])
+	# 2. Initialize a new time entry for the project
+		# @time_entry = @project.time_entries.new
+	# 3. Save the time entry
+		# @time_entry.save
