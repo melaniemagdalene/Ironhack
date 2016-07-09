22070788d336767d4bd822bbc2378d030c935f09
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-09 11:20:36 -0400

    Generated controllers and models

---
+++ Week_11/concert_network/db/seeds.rb
@@ -0,0 +1,14 @@
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+concerts = [
+	["Enrique Iglesias", "American Airlines Arena", "Miami", DateTime.current, 250, "Sex and Love Tour"]
+	["Justin Bieber", "Console Energy Center", "Pittsburgh", DateTime.current, 200, "Purpose World Tour"]
+	["Pitbull", "BB&T Center", "Sunrise", DateTime.current, 150, "The Bad Man Tour"]
+	["Joan Jett", "Perfect Vodka Amphitheatre", "West Palm Beach", DateTime.current, 100, "Rock Hall Three For All"]
+	]
