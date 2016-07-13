ba35fdc2f4d3b6cebcdfe1c50f741d8bfefafbed
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-12 21:48:59 -0400

    Created the viking pit

--- Week_13/viking_exercise.js
+++ Week_13/viking_exercise.js
@@ -1,17 +1,17 @@
-var Viking = function(name, health, attack){
+var Viking = function(name, health, strength){
 	this.name = name;
 	this.health = health;
-	this.attack = attack;
+	this.strength = strength;
 
-	this.attack = function(){
-		console.log(this.attack + "!!!");
+	this.attack = function(defender){
+		console.log(defender.health = defender.health - this.strength);
 	}
 };
 
-var viking1 = new Viking("Josh", 100, 8);
-var viking2 = new Viking("Faraz", 100, 5);
+var viking1 = new Viking("Josh", 100, 10);
+var viking2 = new Viking("Faraz", 200, 5);
 
 viking1.attack(viking2);
-
-console.log(viking2.health);
-
+viking2.attack(viking1);
+viking1.attack(viking2);
+viking2.attack(viking1);
