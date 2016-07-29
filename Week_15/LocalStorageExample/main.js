aa91c40c41cf30f1d103f578e7b7108759b605ec
melaniemagdalene <melanieblazevic@icloud.com>
2016-07-28 21:08:43 -0400

    Local storage exercise

---
+++ Week_15/LocalStorageExample/main.js
@@ -0,0 +1,44 @@
+$(document).ready(function() {
+	var $color_button = $(".color-changer");
+
+	function loadDefaultColor(){
+	  if (window.localStorage.getItem("bg-color")){
+	    var savedColor = window.localStorage.getItem("bg-color");
+	    $("body").css("background-color", savedColor)
+	  }
+	}
+
+	function changeColor(event){
+	  var color = $(event.target).data("color");
+	  $("body").css("background-color", color);
+	  window.localStorage.setItem("bg-color", color)
+	}
+
+	loadDefaultColor();
+	$color_button.on("click", changeColor);
+
+
+	if (window.localStorage.getItem("images")) {
+		// Get the urls
+		// Append urls to the page
+	}	else {
+		window.localStorage.setItem("images", "[]");
+	}
+
+	$(".js-image-button").on("click", function() {
+		var imageUrl = prompt("Enter a URL to display your favorite image:");
+		var myImage = `
+			<img src=${imageURL}>
+		`;
+		$(".image-container").append(myImage);
+
+		// Get the array out of localStorage, and parse it as JSON
+		var storedImages = JSON.parse(window.localStorage.getItem("images"))
+		
+		// Push the new image to that array
+		storedImages.push(imageUrl);
+
+		// Put the new array back in localStorage
+		localStorage.setItem("images", JSON.stringify(storedImages));
+	})
+})
