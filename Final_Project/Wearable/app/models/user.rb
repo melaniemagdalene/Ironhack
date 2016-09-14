1344830ef1d81f378abdfd22305b8226e1a2e2db
melaniemagdalene <melanieblazevic@icloud.com>
2016-09-14 00:08:21 -0400

    Generated Sessions controller, configured routes and created page templates

--- Final_Project/Wearable/app/models/user.rb
+++ Final_Project/Wearable/app/models/user.rb
@@ -1,10 +1,8 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-  devise :database_authenticatable, :registerable,
+	devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
-	attr_accessor :name, :email
+	
 	has_many :posts
-
-	validates :name, presence: true
 end
--- Final_Project/Wearable/config/routes.rb
+++ Final_Project/Wearable/config/routes.rb
@@ -1,15 +1,25 @@
 Rails.application.routes.draw do
-  devise_for :users
-	# get 'home/index'
+ #  get 'sessions/new'
 
-	# get 'users/new'
-	# get 'home/index'
+ #  devise_for :users
+	# # get 'home/index'
 
-	root 'home#index'
+	# # get 'users/new'
+	# # get 'home/index'
+
+	# root 'home#index'
+
+	root   'static_pages#home'
+	get    '/help',    to: 'static_pages#help'
+	get    '/about',   to: 'static_pages#about'
+	get    '/contact', to: 'static_pages#contact'
+	get    '/signup',  to: 'users#new'
+	get    '/login',   to: 'sessions#new'
+	post   '/login',   to: 'sessions#create'
+	delete '/logout',  to: 'sessions#destroy'
+
 
 	resources :users do
 	resources :posts
 	end
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
