b5a8aea694a0b42f089015d593436dc68ba9738f
melaniemagdalene <melanieblazevic@icloud.com>
2016-09-17 17:25:54 -0400

    Fixed destroy function

--- Final_Project/Wearable/config/routes.rb
+++ Final_Project/Wearable/config/routes.rb
@@ -8,13 +8,13 @@ Rails.application.routes.draw do
 	get    '/about',   to: 'static_pages#about'
 	get    '/contact', to: 'static_pages#contact'
 	get    '/signup',  to: 'users#new'
-	get    '/login',   to: 'sessions#new'
-	post   '/login',   to: 'sessions#create'
-	delete '/logout',  to: 'sessions#destroy'
+	# get    '/login',   to: 'sessions#new'
+	# post   '/login',   to: 'sessions#create'
+	# delete '/logout',  to: 'sessions#destroy'
 	
-	devise_scope :user do
-		get '/signout', to: 'devise/sessions#destroy', as: :signout
-	end
+	# devise_scope :user do
+	# 	get '/signout', to: 'devise/sessions#destroy', as: :signout
+	# end
 
 	# resources :users 
 	resources :posts
