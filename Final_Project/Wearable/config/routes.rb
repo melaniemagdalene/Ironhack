78a67b756864ac927b8a4cfa58bc5fdc30656462
melaniemagdalene <melanieblazevic@icloud.com>
2016-09-17 17:49:52 -0400

    UI updates

--- Final_Project/Wearable/config/routes.rb
+++ Final_Project/Wearable/config/routes.rb
@@ -2,21 +2,12 @@ Rails.application.routes.draw do
 
   devise_for :users
 
-
 	root   'static_pages#home'
 	get    '/help',    to: 'static_pages#help'
 	get    '/about',   to: 'static_pages#about'
 	get    '/contact', to: 'static_pages#contact'
+	get    '/posts', to: 'static_pages#post'
 	get    '/signup',  to: 'users#new'
-	# get    '/login',   to: 'sessions#new'
-	# post   '/login',   to: 'sessions#create'
-	# delete '/logout',  to: 'sessions#destroy'
-	
-	# devise_scope :user do
-	# 	get '/signout', to: 'devise/sessions#destroy', as: :signout
-	# end
-
-	# resources :users 
-	resources :posts
-	
+ 
+	resources :posts	
 end
