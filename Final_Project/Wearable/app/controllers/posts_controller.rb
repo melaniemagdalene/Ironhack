832a7c2214e0aa7476d4c962a5718a56d092a270
melaniemagdalene <melanieblazevic@icloud.com>
2016-09-20 20:44:41 -0400

    Reformatting forms and adjusting stylesheet

--- Final_Project/Wearable/app/controllers/posts_controller.rb
+++ Final_Project/Wearable/app/controllers/posts_controller.rb
@@ -7,17 +7,28 @@ class PostsController < ApplicationController
 	end
 
 	def show
+	    @user = User.find(session[:user_id])
+	    @signed_in_user = session[:user_id]
+
+	    @post = Post.find params[:id]
+	    @description = Description.new(post: @post)
 	end
 
 	def new
-		@post = Post.find(params[:post_id])
-		@post = @user.posts.new
+		@post = Post.new
 	end
 
 	def edit
 	end
 
 	def create
+		# @post = Post.new(post_params)
+        
+	#      if @post.save
+	 #        redirect_to '/posts'
+	 #    else
+	 #    	render "new"
+	 #    end
 	end
 
 	def update
--- Final_Project/Wearable/config/routes.rb
+++ Final_Project/Wearable/config/routes.rb
@@ -6,7 +6,7 @@ Rails.application.routes.draw do
 	get    '/help',    to: 'static_pages#help'
 	get    '/about',   to: 'static_pages#about'
 	get    '/contact', to: 'static_pages#contact'
-	get    '/posts', to: 'static_pages#post'
+	get    '/posts/new', to: 'static_pages#posts'
 	get    '/signup',  to: 'users#new'
  
 	resources :posts	
